ecb1e1966639602bfe17656ef528085ea31c347d
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-08-22 15:00:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function toFahrenheit(celsius) {
  // Write your code here
+	let fahrenheit = (celsius * 9/5) + 32;
+    // Return the result rounded to two decimal places
+    return fahrenheit.toFixed(2);
 }
 
 // Do not change the code below
